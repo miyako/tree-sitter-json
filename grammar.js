c62673987c17ed2aca6e3165e90993377b2629e9
Keisuke Miyako <keisuke.miyako@4d.com>
2020-05-21 09:38:41 +0900

    at fourd prefix

--- grammar.js
+++ grammar.js
@@ -1,5 +1,5 @@
 module.exports = grammar({
-  name: 'json',
+  name: 'fourdjson',
 
   extras: $ => [
     /\s/
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 try {
-  module.exports = require("./build/Release/tree_sitter_json_binding");
+  module.exports = require("./build/Release/tree_sitter_fourdjson_binding");
 } catch (error) {
   try {
-    module.exports = require("./build/Debug/tree_sitter_json_binding");
+    module.exports = require("./build/Debug/tree_sitter_fourdjson_binding");
   } catch (_) {
     throw error
   }
